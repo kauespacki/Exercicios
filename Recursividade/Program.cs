826aec7137550d7a0b7f1b860a9148534649aee5
Kaue <kauehspacki@gmail.com>
2024-02-18 10:45:41 -0300

    Projeto recursividade com exemplo de Fatorial e Fibonacci

---
+++ Recursividade/Program.cs
@@ -0,0 +1,33 @@
+﻿// Resolvendo Fatorial com recursividade
+
+/*Console.Write("Entre com um valor: ");
+var valor = int.Parse(Console.ReadLine());
+Console.WriteLine($"O fatorial é {Fatorial(valor)}");
+
+int Fatorial(int x) {
+    // caso-base
+    if (x <= 1)
+    {
+        return 1;
+    }
+
+    return x * Fatorial(x - 1);
+}*/
+
+// Resolvendo Fibonacci com recursividade
+// 0 1 1 2 3 5 8 1 3 2 1
+
+Console.Write("Entre com a posição: ");
+var posicao = int.Parse(Console.ReadLine());
+Console.WriteLine($"O fibonacci na posição {posicao} é {Fibonacci(posicao)}");
+
+int Fibonacci(int x)
+{
+    // caso-base
+    if (x == 0 || x == 1)
+    {
+        return x;
+    }
+
+    return Fibonacci(x - 1) + Fibonacci(x - 2);
+}
